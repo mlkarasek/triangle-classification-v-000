15cb773fe1661c9deea17eb5e85ff022a2bb00f9
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 20:17:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+
+attr_accessor :side_1, :side_2, :side_3
+
+def initialize(side_1, side_2, side_3)
+  @side_1 = side_1
+  @side_2 = side_2
+  @side_3 = side_3  
+end 
+
+def kind 
+kind = nil 
+end 
+
 end
